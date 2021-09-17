bd73f3e9154a66a9b8f012fb71edc40d7a029d8b
Kristen <kristenwood123@gmail.com>
2021-09-16 18:08:45 -0700

    added dates in separate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,32 +1,26 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate'
 import '../styles/index.css'
 
 const ExpenseItem = ({ items }) => {
 
-
   return (
-    <div className='expense-item'>
+    <>
       {items.map((item, id) => {
-      const { title, description, amount, date } = item;
-        const month = date.toLocaleString('en-US', { month: 'long'})
-        const year = date.getFullYear()
-        const day = date.toLocaleString('en-US', { day: '2-digit'})
+        const { title, description, amount, date } = item;
         return (
           <>
-          <div key={id}>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
-          </div>
+          <div key={id} className='expense-item'>
+             <ExpenseDate date={date} />
           <div className="expense-item__description">{description}
             <h2>{title}</h2>
           </div>
           <div className="expense-item__price">{amount}</div>
-        
+          </div>
         </>
         )
       })}
-    </div>
+    </>
      )
 }
 
