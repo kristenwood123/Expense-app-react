4525905efc27c33a2abeb899aa5b4fb4113b4211
Kristen <kristenwood123@gmail.com>
2021-09-16 17:51:31 -0700

    formatted dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,28 @@ import '../styles/index.css'
 
 const ExpenseItem = ({ items }) => {
 
+
   return (
     <div className='expense-item'>
       {items.map((item, id) => {
-        return <h1>{item.title}</h1>
+      const { title, description, amount, date } = item;
+        const month = date.toLocaleString('en-US', { month: 'long'})
+        const year = date.getFullYear()
+        const day = date.toLocaleString('en-US', { day: '2-digit'})
+        return (
+          <>
+          <div key={id}>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+          </div>
+          <div className="expense-item__description">{description}
+            <h2>{title}</h2>
+          </div>
+          <div className="expense-item__price">{amount}</div>
+        
+        </>
+        )
       })}
     </div>
      )
